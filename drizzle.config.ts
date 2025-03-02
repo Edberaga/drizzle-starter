eed22117d15c44bc4abf52632e8aa190c95ede8b
edberaga <edbertjonnathan@gmail.com>
2025-03-03 04:01:45 +0700

    configure the migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,11 @@
+import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
+config({ path: '.env'});
 
 export default defineConfig({
 	schema: './src/lib/server/db/schema.ts',
-
+	out: './migrations',
 	dbCredentials: {
 		url: process.env.DATABASE_URL
 	},
