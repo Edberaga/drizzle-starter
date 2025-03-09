34b7184a0c539a4bbd6e5d732f257f6a20870ece
edberaga <edbertjonnathan@gmail.com>
2025-03-09 18:18:06 +0700

    Fix the connection

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,7 +1,9 @@
 import { drizzle } from 'drizzle-orm/neon-http';
 import { neon } from '@neondatabase/serverless';
 import { env } from '$env/dynamic/private';
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const client = neon(env.DATABASE_URL);
-console.log(env.DATABASE_URL);
+import { config } from 'dotenv';
+
+config({ path: '.env'});
+const client = neon(process.env.DATABASE_URL!);
+console.log(client);
 export const db = drizzle(client);
